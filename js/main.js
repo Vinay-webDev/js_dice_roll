d2fdb6b043cb7ae272433dca24d042a94de39233
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-02 23:56:31 +0530

    javascript

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function rollDice() {
+    const dice = document.getElementById("dice");
+    let randomNumber = Math.floor(Math.random()*6) + 1;
+    dice.textContent = randomNumber;
+}
+
+
+
+
+
+
+
+
+
+
+
+
